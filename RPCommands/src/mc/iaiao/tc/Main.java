89f1d8e90455661f1d04bb954ac7bfb7eebfb71b
Iaiao <err266228@gmail.com>
2021-01-30 17:07:58 +0200

    Make RPCommands work with Spigot

--- RPCommands/src/mc/iaiao/tc/Main.java
+++ RPCommands/src/mc/iaiao/tc/Main.java
@@ -1,6 +1,8 @@
 package mc.iaiao.tc;
 
 import org.bukkit.command.CommandMap;
+
+import java.lang.reflect.Field;
 import java.util.Objects;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,9 +14,23 @@ public class Main extends JavaPlugin {
                 continue;
             }
             this.getLogger().info("Loading command " + cmd);
-            final CommandMap commands = this.getServer().getCommandMap();
+            final CommandMap commands = getCommandMap();
             final org.bukkit.command.Command command = new Command(cmd, Objects.requireNonNull(this.getConfig().getString(cmd + ".type")), this);
             commands.register(cmd, command);
+        }
+    }
+    
+    public CommandMap getCommandMap() {
+        try {
+            Field field = getServer().getClass().getDeclaredField("commandMap");
+            boolean wasAccessible = field.isAccessible();
+            field.setAccessible(true);
+            CommandMap map = (CommandMap) field.get(getServer());
+            field.setAccessible(wasAccessible);
+            return map;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Cannot get CraftServer#commandMap");
         }
     }
 }
